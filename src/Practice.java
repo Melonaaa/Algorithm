798a8751277190e8f67f47e93b3e17066f600375
GilJun <unity.gggg@gmail.com>
2022-06-22 09:03:47 +0900

    Upload Practice.java

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,30 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Practice p = new Practice();
+        System.out.println(p.solution(9));
+    }
+
+    int solution(int n) {
+        int result = 0;
+        int count = getBinaryCount(n);
+        for (int i = 1; i < n; i++) {
+            if(count==getBinaryCount(i)){
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private int getBinaryCount(int n) {
+        String s = Integer.toBinaryString(n);
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                count = count + 1;
+            }
+        }
+        return count;
     }
 }
+
+
