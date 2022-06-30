748add1667d3f4116e34a6416b95bf11ca72fe36
GilJun <unity.gggg@gmail.com>
2022-06-30 13:13:36 +0900

    20220630|키패드 누르기_ãL1

--- src/Practice.java
+++ src/Practice.java
@@ -1,29 +1,62 @@
 public class Practice {
     public static void main(String[] args) {
         Practice p = new Practice();
-        System.out.println(p.solution(9));
+
+        int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+        int[] numbers2 = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
+
+        System.out.println(p.solution(numbers, "right"));
+        System.out.println(p.solution(numbers2, "left"));
     }
 
-    int solution(int n) {
-        int result = 0;
-        int count = getBinaryCount(n);
-        for (int i = 1; i < n; i++) {
-            if(count==getBinaryCount(i)){
-                result++;
+    String solution(int[] numbers, String hand) {
+        StringBuilder sb = new StringBuilder();
+        int leftIndex = 10;
+        int rightIndex = 12;
+
+        for(int number : numbers){
+            if(number == 1 || number == 4 || number == 7){
+                leftIndex = number;
+                sb.append("L");
+            }else if(number == 3 || number == 6 || number == 9){
+                rightIndex = number;
+                sb.append("R");
+            }else{
+                int leftDist = getDistance(leftIndex, number);
+                int rightDist = getDistance(rightIndex, number);
+
+                if(leftDist < rightDist){
+                    leftIndex = number;
+                    sb.append("L");
+                }else if(leftDist > rightDist){
+                    rightIndex = number;
+                    sb.append("R");
+                }else{
+                    if(hand.equals("left")){
+                        leftIndex = number;
+                        sb.append("L");
+                    }else{
+                        rightIndex = number;
+                        sb.append("R");
+                    }
+                }
             }
         }
-        return result;
+
+        return sb.toString();
     }
 
-    private int getBinaryCount(int n) {
-        String s = Integer.toBinaryString(n);
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '1') {
-                count = count + 1;
-            }
-        }
-        return count;
+    private int getDistance(int currIndex, int number) {
+        currIndex = (currIndex == 0) ? 11 : currIndex;
+        number = (number == 0) ? 11 : number;
+
+        int currX = (currIndex - 1) / 3;
+        int currY = (currIndex - 1) % 3;
+
+        int numX = (number - 1) / 3;
+        int numY = 1;
+
+        return Math.abs(currX - numX) + Math.abs(currY - numY);
     }
 }
 
